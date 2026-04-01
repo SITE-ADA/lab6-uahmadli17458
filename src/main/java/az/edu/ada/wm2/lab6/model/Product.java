8b6c78ccd2bae52a0cd819af7407598c6a68c4d9
uahmadli17458 <uahmadli17458@ada.edu.az>
2026-04-01 22:53:33 +0400

    Add ProductRepository

--- src/main/java/az/edu/ada/wm2/lab6/model/Product.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/Product.java
@@ -1,13 +1,25 @@
 package az.edu.ada.wm2.lab6.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false)
     private String productName;
+
+    @Column(nullable = false)
     private BigDecimal price;
+
     private LocalDate expirationDate;
 
     // Constructors
@@ -15,7 +27,6 @@ public class Product {
     }
 
     public Product(String productName, BigDecimal price, LocalDate expirationDate) {
-        this.id = UUID.randomUUID();
         this.productName = productName;
         this.price = price;
         this.expirationDate = expirationDate;
